3750681568fa5f3d5d5d1e73c7262a0434e88294
BranxAgencia <agenciabranx@gmail.com>
2025-07-12 14:33:30 -0300

    Subindo site de bolsas

---
+++ script.js
@@ -0,0 +1,230 @@
+const produtos = [
+  {
+    nome: "Mochila 2 em 1",
+    preco: "R$ 260,00",
+    medidas: "Largura 34cm x Altura 32cm",
+    imagens: [
+      "products/1 - Mochila 2 em 1 - a34xl32/capa.jpg",
+      "products/1 - Mochila 2 em 1 - a34xl32/1.jpg",
+      "products/1 - Mochila 2 em 1 - a34xl32/2.jpg",
+      "products/1 - Mochila 2 em 1 - a34xl32/3.jpg",
+      "products/1 - Mochila 2 em 1 - a34xl32/4.jpg",
+      "products/1 - Mochila 2 em 1 - a34xl32/5.jpg",
+      "products/1 - Mochila 2 em 1 - a34xl32/6.jpg"
+    ]
+  },
+  {
+    nome: "Frasqueira luxo",
+    preco: "R$ 200,00",
+    medidas: "Largura 23cm x Altura 26cm",
+    imagens: [
+      "products/2 - Frasqueira luxo - a26xl23/capa.jpg",
+      "products/2 - Frasqueira luxo - a26xl23/1.jpg",
+      "products/2 - Frasqueira luxo - a26xl23/2.jpg",
+      "products/2 - Frasqueira luxo - a26xl23/3.jpg"
+    ]
+  },
+  {
+    nome: "Trocador portátil",
+    preco: "R$ 90,00",
+    medidas: "Padrão",
+    imagens: [
+      "products/3 - Trocador portátil/capa.jpg",
+      "products/3 - Trocador portátil/1.jpg",
+      "products/3 - Trocador portátil/2.jpg",
+      "products/3 - Trocador portátil/3.jpg"
+    ]
+  },
+  {
+    nome: "Mala baú",
+    preco: "R$ 330,00",
+    medidas: "Largura 25cm x Altura 30cm x Comprimento 50cm",
+    imagens: [
+      "products/4 - Mala baú  - a30xc50xl25/capa.jpg",
+      "products/4 - Mala baú  - a30xc50xl25/1.jpg",
+      "products/4 - Mala baú  - a30xc50xl25/2.jpg",
+      "products/4 - Mala baú  - a30xc50xl25/3.jpg",
+      "products/4 - Mala baú  - a30xc50xl25/4.jpg"
+    ]
+  },
+  {
+    nome: "Bolsa maternidade G",
+    preco: "R$ 280,00",
+    medidas: "Largura 15cm x Altura 33cm x Comprimento 42cm",
+    imagens: [
+      "products/5 - Bolsa maternidade G - a33xc42xl15/capa.jpg",
+      "products/5 - Bolsa maternidade G - a33xc42xl15/2.jpg",
+      "products/5 - Bolsa maternidade G - a33xc42xl15/3.jpg",
+      "products/5 - Bolsa maternidade G - a33xc42xl15/4.jpg"
+    ]
+  },
+  {
+    nome: "Bolsa M",
+    preco: "R$ 240,00",
+    medidas: "Largura 14cm x Altura 25cm x Comprimento 38cm",
+    imagens: [
+      "products/6 - Bolsa M - a25xc38xl14/capa.jpg"
+    ]
+  },
+  {
+    nome: "Bolsa P",
+    preco: "R$ 210,00",
+    medidas: "Largura 20cm x Altura 33cm",
+    imagens: [
+      "products/7- Bolsa P - a20xl33/capa.jpg"
+    ]
+  },
+  {
+    nome: "Mochila tradicional",
+    preco: "R$ 230,00",
+    medidas: "Largura 28cm x Altura 32cm",
+    imagens: [
+      "products/9 - Mochila tradicional - a32xl28/capa.jpg"
+    ]
+  },
+  {
+    nome: "Nécessaire porta-lenço e fralda",
+    preco: "R$ 1100,00",
+    medidas: "Padrão",
+    imagens: [
+      "products/10 - Nécessaire porta-lenço e fralda/capa.jpg",
+      "products/10 - Nécessaire porta-lenço e fralda/1.jpg",
+      "products/10 - Nécessaire porta-lenço e fralda/2.jpg"
+    ]
+  },
+  {
+    nome: "Porta-mamadeira",
+    preco: "R$ 180,00",
+    medidas: "Largura 28cm x Altura 23cm",
+    imagens: [
+      "products/11 - Porta-mamadeira - a23xl28/capa.jpg"
+    ]
+  },
+  {
+    nome: "Saquinhos organizadores de troca",
+    preco: "R$ 60,00",
+    medidas: "Padrão",
+    imagens: [
+      "products/12 - Saquinhos organizadores de troca/capa.jpg",
+      "products/12 - Saquinhos organizadores de troca/1.jpg",
+      "products/12 - Saquinhos organizadores de troca/2.jpg",
+      "products/12 - Saquinhos organizadores de troca/3.jpg",
+      "products/12 - Saquinhos organizadores de troca/4.jpg"
+    ]
+  },
+  {
+    nome: "Bolsa da mamãe",
+    preco: "R$ 280,00",
+    medidas: "Largura 38cm x Altura 30cm x Comprimento 16cm",
+    imagens: [
+      "products/13 - Bolsa da mamãe - a30xc38xl16/capa.jpg"
+    ]
+  },
+  {
+    nome: "Kit organizador",
+    preco: "P-R$ 60,00/M-R$ 80,00/G-R$ 90,00",
+    medidas: "Padrão",
+    imagens: [
+      "products/14 - Kit organizador/capa.jpg",
+      "products/14 - Kit organizador/1.jpg",
+      "products/14 - Kit organizador/2.jpg",
+      "products/14 - Kit organizador/3.jpg",
+      "products/14 - Kit organizador/4.jpg"
+    ]
+  },
+  {
+    nome: "Mala tradiciona",
+    preco: "R$ 280,00",
+    medidas: "Largura 43cm x Altura 33cm",
+    imagens: [
+      "products/15 - Mala tradicional - a33xl43/capa.jpg"
+    ]
+  },
+  {
+    nome: "Frasqueira simples",
+    preco: "R$ 100,00",
+    medidas: "Largura 20cm x Altura 20cm x Comprimento 22cm",
+    imagens: [
+      "products/16 - Frasqueira simples - a20xc22xl20/capa.jpg"
+    ]
+  },
+  {
+    nome: "Porta documentos",
+    preco: "R$ 90,00",
+    medidas: "Padrão",
+    imagens: [
+      "products/17 - Porta documentos/capa.jpg"
+    ]
+  },
+  {
+    nome: "Porta-chupeta",
+    preco: "R$ 50,00",
+    medidas: "Padrão",
+    imagens: [
+      "products/18 - Porta-chupeta/capa.jpg"
+    ]
+  },
+];
+
+const catalogo = document.getElementById("catalogo");
+
+produtos.forEach((p, index) => {
+  const card = document.createElement("div");
+  card.className = "bg-white rounded-2xl shadow-md p-4 transition";
+
+  card.innerHTML = `
+   <img src="${p.imagens[0]}" class="w-full h-48 object-cover rounded-xl mb-4" />
+  <h2 class="text-xl font-semibold">${p.nome}</h2>
+  <p class="text-gray-500 text-sm mb-2">${p.medidas}</p>
+  <p class="text-pink-600 font-bold mb-4">${p.preco}</p>
+    <div class="flex flex-col sm:flex-row gap-2">
+      <button onclick="abrirModal(${index})"
+              class="bg-gray-200 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-300 transition w-full">
+        Ver detalhes
+      </button>
+      <a href="https://wa.me/5585986500500" target="_blank"
+   class="bg-pink-500 text-white px-4 py-2 rounded-lg hover:bg-pink-600 transition w-full flex items-center justify-center gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+  <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+</svg>
+  WhatsApp
+</a>
+    </div>
+  `;
+
+  catalogo.appendChild(card);
+});
+
+function abrirModal(index) {
+  const produto = produtos[index];
+
+  document.getElementById("modal-nome").textContent = produto.nome;
+  document.getElementById("modal-preco").textContent = produto.preco;
+  document.getElementById("modal-medidas").textContent = produto.medidas;
+
+  const container = document.getElementById("modal-imagens");
+  container.innerHTML = "";
+
+  produto.imagens.forEach((src) => {
+    const img = document.createElement("img");
+    img.src = src;
+    img.className = "h-full w-4/5 object-cover rounded-xl cursor-pointer hover:scale-105 transition flex-shrink-0";
+    img.onclick = () => abrirImagemExpandida(src);
+    container.appendChild(img);
+  });
+
+  document.getElementById("modal").classList.remove("hidden");
+}
+
+function abrirImagemExpandida(src) {
+  document.getElementById("zoom-img").src = src;
+  document.getElementById("zoom-modal").classList.remove("hidden");
+}
+
+function fecharZoom() {
+  document.getElementById("zoom-modal").classList.add("hidden");
+}
+
+function fecharModal() {
+  document.getElementById("modal").classList.add("hidden");
+}
